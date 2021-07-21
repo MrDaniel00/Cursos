d91f1aa74a7a0fd4d5202f7c07a80b3cda9e2c59
Nicolas.Diaz <ndiaz@bidda.net>
2021-07-21 13:04:09 -0500

    Used CSS and Boostrap

--- Curso_Angular/angular-proyecto/src/app/personas/personas.component.ts
+++ Curso_Angular/angular-proyecto/src/app/personas/personas.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
  @Component({
    selector:'app-personas',
-   templateUrl: './personas.component.html'
+   templateUrl: './personas.component.html',
+   styleUrls :[ './personas.Component.css' ]
 
  })
  export class PersonasComponent{
