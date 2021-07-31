e551a495d333dbc5d935b0e71f9f37a1e17df258
Nicolas.Diaz <ndiaz@bidda.net>
2021-07-31 12:00:02 -0500

    conect to firebases

---
+++ Curso_Angular/Proyecto Conect_databases/src/app/personas/personas.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Persona } from '../persona.model';
+import { LoggingService } from '../LoggingService.service';
+import { PersonasService } from '../personas.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { routerNgProbeToken } from '@angular/router/src/router_module';
+
+@Component({
+  selector: 'app-personas',
+  templateUrl: './personas.component.html',
+  styleUrls: ['./personas.component.css']
+})
+export class PersonasComponent implements OnInit {
+
+  personas: Persona[] = [];
+  
+  constructor(private loggingService: LoggingService,
+              private personasService: PersonasService,
+              private router: Router,
+              private route:ActivatedRoute
+              ){}
+    
+    ngOnInit(): void {
+     this.personas = this.personasService.personas;
+    }
+
+    irAgregar(){
+      console.log("nos vamos a agregar ");
+      this.router.navigate(['./personas/agregar'],{queryParams:{modoEdicion:0}});
+    }
+}
