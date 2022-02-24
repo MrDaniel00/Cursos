c0b1689e63a2b5af64a64de1ac130ab872de4394
Daniel Diaz <diaznicolas9805@gmail.com>
2022-02-23 21:34:40 -0500

    Add files via upload

---
+++ Lern_Python_FirstSteps/Bucles/Bucles.py
@@ -0,0 +1,8 @@
+
+numero = int(input("ingrese un numero para multiplicarlo "))
+i = 0
+mult= 0
+while i <= 10:
+    mul = numero* i
+    print("{} * {} = {}".format(numero,i,mult))
+    i = i+1
---
+++ Lern_Python_FirstSteps/Cadenas/Cadenas.py
@@ -0,0 +1,15 @@
+vocal = input("inserte una vocal minuscula ")
+letra = input("inserte una letra en mayuscula ")
+
+print("esta vocal "+vocal+" se convertira en mayuscula",vocal.upper())
+print("esta letra "+letra+ " se convertira en minuscula",letra.lower())
+print(vocal+letra)
+#ejercicio 2
+
+nombre = input("ingrese su nombre: ")
+edad =int(input("ingrese su edad " ))
+sexo = input("ingrese su sexo ")
+
+print("hola, te llamas:"+nombre)
+print("tu edad es: ", edad)
+print("eres:"+sexo)
